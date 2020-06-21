183dedab421108139434fe2d4fe3b6a58ae2659c
KaloyanYosifov <kaloqn665@gmail.com>
2020-06-21 16:04:56 +0300

    Support adding expenses and show list of expenses before proceeding to next stage

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 type Account struct {
 	income int
-	expenses map[string]int
-	splits map[string]int
+	expenses, splits map[string]int
 }
 
 func (account *Account) scanExpenses() bool {
 	var expense string
 
-	fmt.Println("Please enter expenses! In this format \"type:amount -- Example: Food:300\"")
-	fmt.Scan(&expense)
+	fmt.Println("Please enter expenses! In this format \"type:amount -- Example: Food:300\"! You can also finish with expenses by typing finish!")
+	_, err := fmt.Scan(&expense)
+	
+	if err != nil {
+		return false
+	}
 
-	if strings.Contains(expense, "exit") {
+	if strings.Contains(expense, "finish") {
 		return true
 	}
 
@@ -25,7 +29,30 @@ func (account *Account) scanExpenses() bool {
 		fmt.Println("Format for expense is not compatible!")
 		return false
 	}
+	
+	splittedExpense := strings.Split(expense, ":")
+	name := splittedExpense[0]
+	amount, err := strconv.Atoi(splittedExpense[1])
 
+	if err != nil {
+		return false
+	}
+
+	_, ok := account.expenses[name]
+
+	// if we do not have the name in the expense map
+	// just set it
+	if ok == false {
+		account.expenses[name] = amount
+		return false
+	}
+
+	account.expenses[name] += amount
+
+	return false
+}
+
+func (account *Account) scanPercentages() bool {
 	return false
 }
 
@@ -38,7 +65,10 @@ const (
 )
 
 func main() {
-	account := Account{}
+	account := Account{
+		expenses: make(map[string]int),
+		splits: make(map[string]int),
+	}
 	stage := income
 
 	for account.income == 0 {
@@ -52,10 +82,22 @@ func main() {
 		if stage == expense {
 			if account.scanExpenses() {
 				stage = percentageSplit
+
+				fmt.Println("These are the expenses you entered")
+
+				for name, amount := range account.expenses {
+					fmt.Printf("%v - %vlv\n", name, amount)
+				}
+
+				fmt.Println("Press any key to continue!")
+				fmt.Scan()
+
 				continue
 			}
 		} else if stage == percentageSplit {
-
+			if account.scanPercentages() {
+				break
+			}
 		}
 	}
 }
